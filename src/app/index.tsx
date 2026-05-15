7d8dda13329e85f4ceaf72c102d58491c3fe5953
Amadou Ba <baamadou947@gmail.com>
2026-05-15 18:51:27 +0000

    feat: refactor Index component to manage todo items and enhance layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,29 +13,51 @@ import {
 } from "react-native";
 import CarteUtilisateur from "../composants/CarteUtilisateur";
 import { useRouter } from "expo-router";
-import Feather from '@expo/vector-icons/Feather';
+import Feather from "@expo/vector-icons/Feather";
+import TodoItem from "../composants/TodoItem";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type Todo = {
+  id: number;
+  titre: string;
+};
 
 export default function Index() {
-  const [valeurText, setValeurText] = useState("");
+  const [valeurText, setValeurText] = useState<string>("");
+  const [taches, setTaches] = useState<Todo[]>([]);
+
+  const ajouterTache = () => {
+    const nouvelleTache = {
+      id: Math.random(),
+      titre: valeurText,
+    };
+    setTaches([nouvelleTache, ...taches]);
+    setValeurText("");
+  };
+
   return (
-    <View style={styles.ecranPrincipal}>
+    <SafeAreaView style={styles.ecranPrincipal}>
       <TextInput
         style={styles.input}
         value={valeurText}
         onChangeText={setValeurText}
         placeholder="Ecrit une tache"
       />
-      <View>
-        <Feather name="check-square" size={24} color="black" />
-      </View>
-    </View>
+      {taches.map((t) => (
+        <TodoItem key={t.id} valeurText={t.titre} />
+      ))}
+      <Pressable style={styles.button} onPress={ajouterTache}>
+        <Feather name="plus" size={24} color="black" />
+        <Text>Enregistrer</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   ecranPrincipal: {
     flex: 1,
-    paddingTop: 50,
+    paddingTop: 10,
     paddingHorizontal: 15,
   },
   input: {
@@ -43,5 +65,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#bebaba",
     borderRadius: 15,
     paddingHorizontal: 20,
+  },
+  button: {
+    flexDirection: "row",
+    backgroundColor: "#bebaba",
+    padding: 15,
+    borderRadius: 15,
+    marginTop: "auto",
   },
 });
